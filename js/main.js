47d4bc6242ec9540a3a3762fea18f3a65dd5802d
sasi <sasi30379@gmail.com>
2025-06-09 18:12:40 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+      duration: 800,       // animation duration in ms
+      once: true,          // animation triggers once
+      easing: 'ease-in-out'
+    });
+  });;
+
